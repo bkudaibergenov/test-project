b3e2db46c622d6ce4b6f056cfeced72f9d45dff5
derbisshyngys <derbis7212@gmail.com>
2021-05-20 14:35:18 +0600

    CSV to JSON Transactional 2

--- src/main/java/com/example/demo/repository/ContactRepository.java
+++ src/main/java/com/example/demo/repository/ContactRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
